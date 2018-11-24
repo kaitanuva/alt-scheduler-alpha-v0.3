fc2c36a07e3ffc545f686edb6d8e0047a145ec14
Mark <mark.lee1933@gmail.com>
2018-11-24 17:24:27 -0500

    server routing troubleshooting: express static should point to /dist not /dist/index.html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
