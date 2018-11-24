33d9ec809ed8a4b74d95a3891e269d7f3f581a71
Mark <mark.lee1933@gmail.com>
2018-11-24 17:18:51 -0500

    server routing troubleshooting: reverted to old code, this time without alt-scheduler dir

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// app.use(express.static(__dirname + '/dist/alt-scheduler/index.html'));
+app.use(express.static(__dirname + '/dist/index.html'));
 
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/dist/alt-scheduler/index.html'));
-// });
-
-app.get('/', function (req, res) {
-  res.sendfile('./dist/index.html'); 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
