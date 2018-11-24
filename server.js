ebbc6c37757c6f3180de02044218aaa9397619bc
Mark <mark.lee1933@gmail.com>
2018-11-24 16:42:25 -0500

    in server.js directory was mistyped

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/alt-scheduler/index.html'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/alt-scheduler/index.html/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/alt-scheduler/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
